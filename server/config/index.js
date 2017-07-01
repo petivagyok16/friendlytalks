da185e7cbf491166e3c6b4ba1feb5c960a353f53
peter palocz <peter.palocz91@gmail.com>
2017-07-01 16:22:33 +0200

    chore: change indentation

--- server/config/index.js
+++ server/config/index.js
@@ -6,8 +6,8 @@ var configValues = require('./config.json');
 
 module.exports = {
 
-    getDbConnectionString: function () {
-        return 'mongodb://'+ configValues.username + ':' + configValues.password + '@ds015636.mlab.com:15636/friendlytalks';
-    }
+	getDbConnectionString: function () {
+		return 'mongodb://' + configValues.username + ':' + configValues.password + '@ds015636.mlab.com:15636/friendlytalks';
+	}
 
 };
