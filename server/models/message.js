a9ee6dad34b2d438cba23534aa925b1c9ac6da0e
peter palocz <peter.palocz91@gmail.com>
2017-07-03 16:23:02 +0200

    chore: change indentation

--- server/models/message.js
+++ server/models/message.js
@@ -4,24 +4,24 @@ var User = require('./user');
 
 var schema = new Schema({
 
-    content: { type: String, required: true },
-    created_at: { type: Date, default: Date.now },
-    meta: {
-            likes: [{ type: Schema.Types.ObjectId, ref: 'User'}],
-            dislikes: [{ type: Schema.Types.ObjectId, ref: 'User'}]
-    },
-    user: { type: Schema.Types.ObjectId, ref: 'User'}
+	content: { type: String, required: true },
+	created_at: { type: Date, default: Date.now },
+	meta: {
+		likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+		dislikes: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+	},
+	user: { type: Schema.Types.ObjectId, ref: 'User' }
 
 });
 
 //post method means this should be fired after something. It has nothing to do with post request
 //So after removing something do this:
 schema.post('remove', function (doc) {
-    var deletedMessage = doc;
-    User.findById(doc.user, function (err, doc) {
-        doc.messages.pull(deletedMessage);
-        doc.save();
-    });
+	var deletedMessage = doc;
+	User.findById(doc.user, function (err, doc) {
+		doc.messages.pull(deletedMessage);
+		doc.save();
+	});
 });
 
 module.exports = mongoose.model('Message', schema);
