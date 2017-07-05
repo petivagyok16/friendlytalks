8ec915cd9cf1da3ec7efbaacaf73910f4327f928
peter palocz <peter.palocz91@gmail.com>
2017-07-05 16:23:34 +0200

    chore: change indentation

--- src/app/announcements.component.ts
+++ src/app/announcements.component.ts
@@ -14,16 +14,14 @@ import { Component, OnInit } from '@angular/core';
     <h4 class="media-heading">@Username</h4>
     <div class="col-md-10 col-sm-10 col-xs-10 message-content">Some mes sage Some message  Sssage  Some message Some  Sssage  Some </div>
     <div class="col-md-2 col-sm-2 col-xs-2 pull-right right-align message-config">
-                          <div class="message-config">
-                              <i class="glyphicon glyphicon-pencil cursor-pointer left-align" data-toggle="tooltip" title="Edit" (click)="onEdit(messageItem)"> </i> 
-                              <i class="glyphicon glyphicon-remove glyphicon-remove-color cursor-pointer left-align" data-toggle="tooltip" title="Remove" (click)="onDelete(messageItem)"> </i>
-                          </div>
-                      </div>
+      <div class="message-config">
+        <i class="glyphicon glyphicon-pencil cursor-pointer left-align" data-toggle="tooltip" title="Edit" (click)="onEdit(messageItem)"> </i> 
+        <i class="glyphicon glyphicon-remove glyphicon-remove-color cursor-pointer left-align" data-toggle="tooltip" title="Remove" (click)="onDelete(messageItem)"> </i>
+      </div>
+    </div>
     <div class="col-md-12 col-sm-12 col-xs-12 message-footer">
-                    
-                       <div class="col-md-6 col-sm-6 col-xs-6 left-align"> panel ratings </div>
-                     <div class="col-md-6 col-sm-6 col-xs-6 right-align"> panel date</div>
-                    
+      <div class="col-md-6 col-sm-6 col-xs-6 left-align"> panel ratings </div>
+      <div class="col-md-6 col-sm-6 col-xs-6 right-align"> panel date</div> 
     </div>
 </div>
 </div>
@@ -33,6 +31,6 @@ import { Component, OnInit } from '@angular/core';
 export class AnnouncementsComponent implements OnInit {
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 
 }
