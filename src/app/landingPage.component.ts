fd3f909f5406f0a88752aaa6fae47f7b66eda43f
peter palocz <peter.palocz91@gmail.com>
2017-07-07 16:24:03 +0200

    chore: change indentation

--- src/app/landingPage.component.ts
+++ src/app/landingPage.component.ts
@@ -1,20 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'my-landingPage',
-    template: `
-               <div class="container-fluid jumbotron">
-                   <div class="row col-md-4">
-                        <h2> Welcome to Friendly Talks!</h2>
-                        <p>Don't have account?</p>
-                        <a routerLink="/signup"><h3>Sign up!</h3></a>
-                   </div>
-               </div>
+	selector: 'my-landingPage',
+	template: `
+		<div class="container-fluid jumbotron">
+				<div class="row col-md-4">
+						<h2> Welcome to Friendly Talks!</h2>
+						<p>Don't have account?</p>
+						<a routerLink="/signup"><h3>Sign up!</h3></a>
+				</div>
+		</div>
 `
 })
 export class LandingPageComponent implements OnInit {
-    constructor() { }
+	constructor() { }
 
-    ngOnInit() { }
+	ngOnInit() { }
 
 }
