c2e320107b5d96ee36622d36833180836887235d
peter palocz <peter.palocz91@gmail.com>
2017-07-08 16:24:18 +0200

    chore: change indentation

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,32 +1,32 @@
 
-import { NgModule }                         from '@angular/core';
-import { CommonModule }                     from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AuthService }                      from './auth.service';
-import { LogoutComponent }                  from './logout.component';
-import { SigninComponent }                  from './signin.component';
-import { SignupComponent }                  from './signup.component';
+import { AuthService } from './auth.service';
+import { LogoutComponent } from './logout.component';
+import { SigninComponent } from './signin.component';
+import { SignupComponent } from './signup.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        ReactiveFormsModule
-    ],
-    declarations: [
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent
-    ],
-    exports: [
-        LogoutComponent,
-        SigninComponent,
-        SignupComponent
-    ],
-    providers: [
-        AuthService
-    ]
+	imports: [
+		CommonModule,
+		FormsModule,
+		ReactiveFormsModule
+	],
+	declarations: [
+		LogoutComponent,
+		SigninComponent,
+		SignupComponent
+	],
+	exports: [
+		LogoutComponent,
+		SigninComponent,
+		SignupComponent
+	],
+	providers: [
+		AuthService
+	]
 })
 export class AuthModule {
 
