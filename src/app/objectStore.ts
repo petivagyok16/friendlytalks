76d9789e976bf3096308c1aa44aa320e5ff5236d
peter palocz <peter.palocz91@gmail.com>
2017-07-08 16:24:12 +0200

    chore: change indentation

--- src/app/objectStore.ts
+++ src/app/objectStore.ts
@@ -1,9 +1,9 @@
 export class ObjectStore {
-    setObject(key, value) {
-        localStorage.setItem(key, JSON.stringify(value));
-    }
-    getObject(key) {
-        const value = localStorage.getItem(key);
-        return value && JSON.parse(value);
-    }
+	setObject(key, value) {
+		localStorage.setItem(key, JSON.stringify(value));
+	}
+	getObject(key) {
+		const value = localStorage.getItem(key);
+		return value && JSON.parse(value);
+	}
 }
