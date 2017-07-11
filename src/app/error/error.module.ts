334e51760e0c3abf9eee169bc01b88a3d804776d
peter palocz <peter.palocz91@gmail.com>
2017-07-11 16:25:03 +0200

    chore: change indentation

--- src/app/error/error.module.ts
+++ src/app/error/error.module.ts
@@ -1,23 +1,23 @@
 
-import { NgModule }         from '@angular/core';
-import { CommonModule }     from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { ErrorComponent }   from './error.component';
-import { ErrorService }     from './error.service';
+import { ErrorComponent } from './error.component';
+import { ErrorService } from './error.service';
 
 @NgModule({
-    imports: [
-        CommonModule
-    ],
-    declarations: [
-        ErrorComponent
-    ],
-    exports: [
-        ErrorComponent
-    ],
-    providers: [
-        ErrorService
-    ]
+	imports: [
+		CommonModule
+	],
+	declarations: [
+		ErrorComponent
+	],
+	exports: [
+		ErrorComponent
+	],
+	providers: [
+		ErrorService
+	]
 })
 export class ErrorModule {
 
