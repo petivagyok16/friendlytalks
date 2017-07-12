cf218c355ae0c986b2079a2878c359b42eac1d60
peter palocz <peter.palocz91@gmail.com>
2017-07-12 16:25:08 +0200

    chore: change indentation

--- src/app/error/error.service.ts
+++ src/app/error/error.service.ts
@@ -1,14 +1,14 @@
 import { EventEmitter } from '@angular/core';
 
-import { Error }        from './error';
+import { Error } from './error';
 
 export class ErrorService {
-    errorOccured = new EventEmitter<Error>();
+	errorOccured = new EventEmitter<Error>();
 
-    handleError(error: any) {
-        // const ERROROBJ = error.json();
-        console.error(`[ErrorService][HandleError]`, error);
-        const ERRORDATA = new Error(error.title, error.error.message);
-        this.errorOccured.emit(ERRORDATA);
-    }
+	handleError(error: any) {
+		// const ERROROBJ = error.json();
+		console.error(`[ErrorService][HandleError]`, error);
+		const ERRORDATA = new Error(error.title, error.error.message);
+		this.errorOccured.emit(ERRORDATA);
+	}
 }
