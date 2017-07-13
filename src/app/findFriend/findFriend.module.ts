86592ae8d9accf557e4f6a4ca4903df2e864c20b
peter palocz <peter.palocz91@gmail.com>
2017-07-13 16:25:24 +0200

    chore: change indentation

--- src/app/findFriend/findFriend.module.ts
+++ src/app/findFriend/findFriend.module.ts
@@ -1,30 +1,30 @@
-import { NgModule }             from '@angular/core';
-import { CommonModule }         from '@angular/common';
-import { RouterModule }         from '@angular/router';
-import { FormsModule, ReactiveFormsModule }          from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
-import { FindFriendComponent }  from './findFriend.component';
-import { FindFriendService }    from './findFriend.service';
-import { SharedModule }         from '../shared/shared.module';
+import { FindFriendComponent } from './findFriend.component';
+import { FindFriendService } from './findFriend.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule,
-        SharedModule,
-        FormsModule,
-        ReactiveFormsModule
-    ],
-    declarations: [
-        FindFriendComponent
-    ],
-    exports: [
-        FindFriendComponent
-    ],
-    providers: [
-        FindFriendService
-    ]
+	imports: [
+		CommonModule,
+		RouterModule,
+		SharedModule,
+		FormsModule,
+		ReactiveFormsModule
+	],
+	declarations: [
+		FindFriendComponent
+	],
+	exports: [
+		FindFriendComponent
+	],
+	providers: [
+		FindFriendService
+	]
 })
 export class FindFriendModule {
 
