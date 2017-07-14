36ef36f920914ea4b4dfbf27e5afc962f35d4ceb
peter palocz <peter.palocz91@gmail.com>
2017-07-14 16:25:29 +0200

    chore: change indentation

--- src/app/findFriend/findFriend.service.ts
+++ src/app/findFriend/findFriend.service.ts
@@ -1,34 +1,34 @@
-import { Injectable }                           from '@angular/core';
-import { Http, Headers, Response }              from '@angular/http';
-import { Observable }                           from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
+import { Observable } from 'rxjs';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/catch';
 
-import { Profile }                              from '../profile/profile';
+import { Profile } from '../profile/profile';
 
 @Injectable()
 export class FindFriendService {
-    private _url: string = 'http://localhost:3000/find';
-    private _token = localStorage.getItem('token') ? '?token=' + localStorage.getItem('token') : '';
+	private _url: string = 'http://localhost:3000/find';
+	private _token = localStorage.getItem('token') ? '?token=' + localStorage.getItem('token') : '';
 
-    constructor(private _http: Http) {}
+	constructor(private _http: Http) { }
 
-    find(username) {
-        return this._http.get(this._url + '/' + username + this._token)
-            .map((response: Response) => {
-                const DATA = response.json().obj;
-                const objs: any[] = [];
+	find(username) {
+		return this._http.get(this._url + '/' + username + this._token)
+			.map((response: Response) => {
+				const DATA = response.json().obj;
+				const objs: any[] = [];
 
-                for (let i = 0; i < DATA.length; i++) {
-                    const foundUser = new Profile(DATA[i].username, DATA[i]._id, DATA[i].messages, DATA[i].email,
-                    DATA[i].pictureUrl, DATA[i].city);
-                    foundUser.name = DATA[i].name;
+				for (let i = 0; i < DATA.length; i++) {
+					const foundUser = new Profile(DATA[i].username, DATA[i]._id, DATA[i].messages, DATA[i].email,
+						DATA[i].pictureUrl, DATA[i].city);
+					foundUser.name = DATA[i].name;
 
-                    objs.push(foundUser);
-                }
-            return objs;
-            })
-            .catch((error: Response) => Observable.throw(error.json()));
-    }
+					objs.push(foundUser);
+				}
+				return objs;
+			})
+			.catch((error: Response) => Observable.throw(error.json()));
+	}
 }
