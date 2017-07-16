a2c862eb784c598c1ccf5ba58c4fc37204082436
peter palocz <peter.palocz91@gmail.com>
2017-07-16 16:25:45 +0200

    chore: change indentation

--- src/app/message/message.module.ts
+++ src/app/message/message.module.ts
@@ -1,31 +1,31 @@
 
-import { NgModule }                         from '@angular/core';
-import { CommonModule }                     from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { RouterModule }                     from '@angular/router';
+import { RouterModule } from '@angular/router';
 
-import { MessageComponent }                 from './message.component';
-import { MessageService }                   from './message.service';
-import { ProfileService }                   from '../profile/profile.service';
-import { SharedModule }                     from '../shared/shared.module';
+import { MessageComponent } from './message.component';
+import { MessageService } from './message.service';
+import { ProfileService } from '../profile/profile.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        ReactiveFormsModule,
-        FormsModule,
-        RouterModule,
-        SharedModule
-    ],
-    declarations: [
-        MessageComponent
-    ],
-    exports: [
-        MessageComponent
-    ],
-    providers: [
-        MessageService,
-        ProfileService
-    ]
+	imports: [
+		CommonModule,
+		ReactiveFormsModule,
+		FormsModule,
+		RouterModule,
+		SharedModule
+	],
+	declarations: [
+		MessageComponent
+	],
+	exports: [
+		MessageComponent
+	],
+	providers: [
+		MessageService,
+		ProfileService
+	]
 })
-export class MessageModule {}
+export class MessageModule { }
