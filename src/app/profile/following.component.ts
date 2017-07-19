597a4e5fb36ab9798ed8636f0860d1a8c2b6ba03
peter palocz <peter.palocz91@gmail.com>
2017-07-19 16:26:04 +0200

    chore: change indentation

--- src/app/profile/following.component.ts
+++ src/app/profile/following.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit }            from '@angular/core';
-import { ActivatedRoute }               from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-import { ProfileService }               from './profile.service';
-import { ErrorService }                 from '../error/error.service';
+import { ProfileService } from './profile.service';
+import { ErrorService } from '../error/error.service';
 
 @Component({
-    selector: 'my-following',
-    template: `
+	selector: 'my-following',
+	template: `
 <h2>Following</h2>
 <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3 margin-top message-panel"  *ngFor="let user of followings">
     <div class="media">
@@ -25,24 +25,27 @@ import { ErrorService }                 from '../error/error.service';
 })
 export class FollowingComponent implements OnInit {
 
-    private followings = [];
-    private userId = null;
-    constructor(private _profileService: ProfileService, private _route: ActivatedRoute, private _errorService: ErrorService) { }
+	private followings = [];
+	private userId = null;
+	constructor(
+		private _profileService: ProfileService,
+		private _route: ActivatedRoute,
+		private _errorService: ErrorService) { }
 
-    ngOnInit() {
+	ngOnInit() {
 
-        // getting the userId from URL param
-        this._route.parent.params
-            .map(params => params['userId'])
-            .subscribe(
-                (userId) => this.userId = userId,
-                error => this._errorService.handleError(error));
+		// getting the userId from URL param
+		this._route.parent.params
+			.map(params => params['userId'])
+			.subscribe(
+			(userId) => this.userId = userId,
+			error => this._errorService.handleError(error));
 
-        this._profileService.getFollowing(this.userId)
-            .subscribe(followings => {
-                this.followings = followings;
-                // console.log(this.followings);
-            },
-            error => this._errorService.handleError(error));
-    }
+		this._profileService.getFollowing(this.userId)
+			.subscribe(followings => {
+				this.followings = followings;
+				// console.log(this.followings);
+			},
+			error => this._errorService.handleError(error));
+	}
 }
