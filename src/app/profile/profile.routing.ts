e6cbaf95c507fe4522878f6b59ad87cc21b23d5c
peter palocz <peter.palocz91@gmail.com>
2017-07-21 16:26:26 +0200

    chore: change indentation

--- src/app/profile/profile.routing.ts
+++ src/app/profile/profile.routing.ts
@@ -1,20 +1,22 @@
-import { RouterModule, Routes }         from '@angular/router';
-import { ModuleWithProviders }          from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ModuleWithProviders } from '@angular/core';
 
-import { ProfileComponent }             from './profile.component';
-import { SelectedProfileComponent }     from './selectedProfile.component';
-import { ProfileUpdateFormComponent }   from './profileUpdateForm.component';
-import { FollowersComponent }           from './followers.component';
-import { FollowingComponent }           from './following.component';
+import { ProfileComponent } from './profile.component';
+import { SelectedProfileComponent } from './selectedProfile.component';
+import { ProfileUpdateFormComponent } from './profileUpdateForm.component';
+import { FollowersComponent } from './followers.component';
+import { FollowingComponent } from './following.component';
 
 const profileRoutes: Routes = [
 
-    { path: 'profile/:userId', children: [
-        { path: '', component: SelectedProfileComponent },
-        // { path: 'update-profile-form', component: ProfileUpdateFormComponent },
-        { path: 'followers', component: FollowersComponent },
-        { path: 'following', component: FollowingComponent },
-    ],component: ProfileComponent },
+	{
+		path: 'profile/:userId', children: [
+			{ path: '', component: SelectedProfileComponent },
+			// { path: 'update-profile-form', component: ProfileUpdateFormComponent },
+			{ path: 'followers', component: FollowersComponent },
+			{ path: 'following', component: FollowingComponent },
+		], component: ProfileComponent
+	},
 ];
 
 export const profileRouting: ModuleWithProviders = RouterModule.forChild(profileRoutes);
