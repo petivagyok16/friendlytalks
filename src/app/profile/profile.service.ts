22d5301d6a71c296a131fb8b9bfc7538ab8b9093
peter palocz <peter.palocz91@gmail.com>
2017-07-22 16:26:31 +0200

    chore: change indentation

--- src/app/profile/profile.service.ts
+++ src/app/profile/profile.service.ts
@@ -1,109 +1,107 @@
-import { Injectable }               from '@angular/core';
-import { Http, Headers, Response }  from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/catch';
-import { Observable }               from 'rxjs';
+import { Observable } from 'rxjs';
 
-import { Profile, UpdatedProfile }  from './profile';
-import { Message }                  from '../message/message';
+import { Profile, UpdatedProfile } from './profile';
+import { Message } from '../message/message';
 
 @Injectable()
 export class ProfileService {
-    private _url: string = 'http://localhost:3000/profile';
-    private _token = localStorage.getItem('token') ? '?token=' + localStorage.getItem('token') : '';
+	private _url: string = 'http://localhost:3000/profile';
+	private _token = localStorage.getItem('token') ? '?token=' + localStorage.getItem('token') : '';
 
-    constructor(private _http: Http) {}
+	constructor(private _http: Http) { }
 
-    // Finding the requested user profiles
-    find(userId) {
-        return this._http.get(this._url + '/' + userId + this._token)
-            .map((response: Response) => {
-                const DATA = response.json().obj;
+	// Finding the requested user profiles
+	find(userId) {
+		return this._http.get(this._url + '/' + userId + this._token)
+			.map((response: Response) => {
+				const DATA = response.json().obj;
 
-                    let profile = new Profile(DATA.username, DATA._id, DATA.messages, DATA.email, DATA.pictureUrl, DATA.city);
-                    profile.name = DATA.name;
-                    profile.relations = DATA.relations;
-                    profile.ratings = DATA.ratings;
+				let profile = new Profile(DATA.username, DATA._id, DATA.messages, DATA.email, DATA.pictureUrl, DATA.city);
+				profile.name = DATA.name;
+				profile.relations = DATA.relations;
+				profile.ratings = DATA.ratings;
 
-                return profile;
-            })
-            .catch((error: Response) => Observable.throw(error.json()));
-    }
+				return profile;
+			})
+			.catch((error: Response) => Observable.throw(error.json()));
+	}
 
-    // by sending the followed and follower user's IDs i can set it server-side to
-    // change both user's relation states in one http request
-    follow(userId, toFollowId, state) {
-        // console.log('userId: ' + userId, 'toFollowId: ' + toFollowId, 'following state: ' + state);
-        const toFollowOrUnfollow = JSON.stringify({ toFollowId: toFollowId, state: state});
-        const HEADERS = new Headers({'Content-Type' : 'application/json'});
+	// by sending the followed and follower user's IDs i can set it server-side to
+	// change both user's relation states in one http request
+	follow(userId, toFollowId, state) {
+		// console.log('userId: ' + userId, 'toFollowId: ' + toFollowId, 'following state: ' + state);
+		const toFollowOrUnfollow = JSON.stringify({ toFollowId: toFollowId, state: state });
+		const HEADERS = new Headers({ 'Content-Type': 'application/json' });
 
-        return this._http.patch(this._url + '/' + userId + this._token, toFollowOrUnfollow, {headers: HEADERS})
-            .map((response: Response) => console.log(response.json()))
-            .catch((error: Response) => Observable.throw(error.json()));
-    }
+		return this._http.patch(this._url + '/' + userId + this._token, toFollowOrUnfollow, { headers: HEADERS })
+			.map((response: Response) => console.log(response.json()))
+			.catch((error: Response) => Observable.throw(error.json()));
+	}
 
-    getFollowers(userId) {
+	getFollowers(userId) {
+		return this._http.get(this._url + '/followers' + '/' + userId + this._token)
+			.map((response: Response) => {
+				const DATA = response.json().obj;
+				const objs: any[] = [];
 
-        return this._http.get(this._url + '/followers' + '/' + userId + this._token)
-            .map((response: Response) => {
-                const DATA = response.json().obj;
-                const objs: any[] = [];
+				for (let i = 0; i < DATA.length; i++) {
+					const follower = new Profile(DATA[i].username, DATA[i]._id, null, DATA[i].email, DATA[i].pictureUrl);
+					follower.name = DATA[i].name;
+					objs.push(follower);
+				}
+				return objs;
+			})
+			.catch((error: Response) => Observable.throw(error.json()));
+	}
 
-                for (let i = 0; i < DATA.length; i++) {
-                    const follower = new Profile(DATA[i].username, DATA[i]._id, null, DATA[i].email, DATA[i].pictureUrl);
-                    follower.name = DATA[i].name;
-                    objs.push(follower);
-                }
-                return objs;
-            })
-            .catch((error: Response) => Observable.throw(error.json()));
-    }
+	getFollowing(userId) {
+		return this._http.get(this._url + '/following' + '/' + userId + this._token)
+			.map((response: Response) => {
+				const DATA = response.json().obj;
+				const objs: any[] = [];
 
-    getFollowing(userId) {
+				for (let i = 0; i < DATA.length; i++) {
+					const following = new Profile(DATA[i].username, DATA[i]._id, [], DATA[i].email, DATA[i].pictureUrl);
+					following.name = DATA[i].name;
 
-        return this._http.get(this._url + '/following' + '/' + userId + this._token)
-            .map((response: Response) => {
-                const DATA = response.json().obj;
-                const objs: any[] = [];
+					objs.push(following);
+				}
+				return objs;
+			})
+			.catch((error: Response) => Observable.throw(error.json()));
+	}
 
-                 for(let i = 0; i < DATA.length; i++) {
-                 const following = new Profile(DATA[i].username, DATA[i]._id, [], DATA[i].email, DATA[i].pictureUrl);
-                     following.name = DATA[i].name;
+	getFollowingMessages(userId) {
+		return this._http.get(this._url + '/followingmessages' + '/' + userId + this._token)
+			.map((response: Response) => {
+				const DATA = response.json().obj;
 
-                     objs.push(following);
-                 }
-                 return objs;
-            })
-            .catch((error: Response) => Observable.throw(error.json()));
-    }
+				const objs: any[] = [];
+				for (let i = 0; i < DATA.length; i++) {
 
-    getFollowingMessages(userId) {
-        return this._http.get(this._url + '/followingmessages' + '/' + userId + this._token)
-            .map((response: Response) => {
-                const DATA = response.json().obj;
+					for (let j = 0; j < DATA[i].messages.length; j++) {
+						let message = new Message(DATA[i].messages[j].content, DATA[i].messages[j].created_at,
+							DATA[i].username, DATA[i].messages[j].meta, DATA[i].messages[j]._id, DATA[i].messages[j].user, DATA[i].pictureUrl);
+						objs.push(message);
+					}
+				}
+				return objs;
+			})
+			.catch((error: Response) => Observable.throw(error.json()));
+	}
 
-                const objs: any[] = [];
-                for (let i = 0; i < DATA.length; i++) {
+	editProfile(userId, profile: UpdatedProfile) {
+		const BODY = JSON.stringify(profile);
+		const HEADERS = new Headers({ 'Content-Type': 'application/json' });
 
-                    for (let j = 0; j < DATA[i].messages.length; j++) {
-                        let message = new Message(DATA[i].messages[j].content, DATA[i].messages[j].created_at,
-                        DATA[i].username, DATA[i].messages[j].meta, DATA[i].messages[j]._id, DATA[i].messages[j].user, DATA[i].pictureUrl);
-                        objs.push(message);
-                    }
-                }
-                return objs;
-            })
-            .catch((error: Response) => Observable.throw(error.json()));
-    }
-
-    editProfile(userId, profile: UpdatedProfile) {
-        const BODY = JSON.stringify(profile);
-        const HEADERS = new Headers({'Content-Type' : 'application/json'});
-
-        return this._http.patch(this._url + '/editprofile' + '/' + userId + this._token, BODY, {headers: HEADERS})
-            .map((response: Response) => response.json())
-            .catch((error: Response) => Observable.throw(error.json()));
-    }
+		return this._http.patch(this._url + '/editprofile' + '/' + userId + this._token, BODY, { headers: HEADERS })
+			.map((response: Response) => response.json())
+			.catch((error: Response) => Observable.throw(error.json()));
+	}
 
 }
