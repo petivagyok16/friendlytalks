07b6ac4130fdc4deddace521c576e80c32e41262
peter palocz <peter.palocz91@gmail.com>
2017-07-23 21:55:09 +0200

    refactor: delete constructor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: `
-                <div class="container-fluid">
-                    <my-navbar></my-navbar>
-                   
-                    <router-outlet></router-outlet>
-                </div>
-                <my-error></my-error>
+	selector: 'my-app',
+	template: `
+		<div class="container-fluid">
+				<my-navbar></my-navbar>
+				
+				<router-outlet></router-outlet>
+		</div>
+		<my-error></my-error>
 
 `
 })
 
 export class AppComponent {
+	constructor() {
 
+	}
 }
