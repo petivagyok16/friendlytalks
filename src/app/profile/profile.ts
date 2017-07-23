f3cc1267458a234bb01aa1bdbc8320090d095e60
peter palocz <peter.palocz91@gmail.com>
2017-07-23 16:26:36 +0200

    chore: change indentation

--- src/app/profile/profile.ts
+++ src/app/profile/profile.ts
@@ -2,47 +2,48 @@ import { Name, User } from '../auth/user';
 
 export class Profile implements User {
 
-    name = new Name();
-    relations = new Relations();
-    ratings = new Ratings();
-    password = null;
+	name = new Name();
+	relations = new Relations();
+	ratings = new Ratings();
+	password = null;
 
-    constructor(public username: string,
-                public id: string,
-                public messages?: any[],
-                public email?: string,
-                public pictureUrl?: string,
-                public city?: string
-                ) {}
+	constructor(
+		public username: string,
+		public id: string,
+		public messages?: any[],
+		public email?: string,
+		public pictureUrl?: string,
+		public city?: string
+	) { }
 }
 
 class Relations {
-    followers: any[] = [];
-    following: any[] = [];
+	followers: any[] = [];
+	following: any[] = [];
 }
 
 export class Ratings {
-    my = new My();
-    given = new Given();
+	my = new My();
+	given = new Given();
 }
 
 class My {
-    likes: any[] = [];
-    dislikes: any[] = [];
+	likes: any[] = [];
+	dislikes: any[] = [];
 }
 
 class Given {
-    likes: any[] = [];
-    dislikes: any[] = [];
+	likes: any[] = [];
+	dislikes: any[] = [];
 }
 
 export class UpdatedProfile {
 
-    constructor(
-                public pictureUrl?: string,
-                public email?: string,
-                public firstName?: string,
-                public lastName?: string,
-                public city?: string
-    ) {}
+	constructor(
+		public pictureUrl?: string,
+		public email?: string,
+		public firstName?: string,
+		public lastName?: string,
+		public city?: string
+	) { }
 }
