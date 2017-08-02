245bcbbd5a44ac1095fa11f6593bb8ab47e8bd3d
peter palocz <peter.palocz91@gmail.com>
2017-08-02 16:27:43 +0200

    chore: change indentation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,31 +1,31 @@
 
-import { NgModule }                 from '@angular/core';
-import { CommonModule }             from '@angular/common';
-import { RouterModule }             from '@angular/router';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-import { FollowComponent }          from './follow.component';
+import { FollowComponent } from './follow.component';
 //import { PaginationComponent }    from './pagination.component';
-import { ProfileBarComponent }      from './profileBar.component';
-import { RatingComponent }          from './rating.component';
-import { SpinnerComponent }         from './spinner.component';
+import { ProfileBarComponent } from './profileBar.component';
+import { RatingComponent } from './rating.component';
+import { SpinnerComponent } from './spinner.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        RouterModule
-    ],
-    declarations: [
-        FollowComponent,
-        ProfileBarComponent,
-        RatingComponent,
-        SpinnerComponent
-    ],
-    exports: [
-        FollowComponent,
-        ProfileBarComponent,
-        RatingComponent,
-        SpinnerComponent
-    ]
+	imports: [
+		CommonModule,
+		RouterModule
+	],
+	declarations: [
+		FollowComponent,
+		ProfileBarComponent,
+		RatingComponent,
+		SpinnerComponent
+	],
+	exports: [
+		FollowComponent,
+		ProfileBarComponent,
+		RatingComponent,
+		SpinnerComponent
+	]
 })
 export class SharedModule {
 
