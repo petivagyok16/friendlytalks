209b0964fe072ddff09893df283f19db2551db21
peter palocz <peter.palocz91@gmail.com>
2017-08-03 16:27:49 +0200

    chore: change indentation

--- src/app/shared/spinner.component.ts
+++ src/app/shared/spinner.component.ts
@@ -1,12 +1,12 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-    selector: 'spinner',
-    template: `<div class="col-md-12 centerAlign"><i *ngIf="visible" class="fa fa-spinner fa-pulse fa-fw fa-3x"></i></div>`
+	selector: 'spinner',
+	template: `<div class="col-md-12 centerAlign"><i *ngIf="visible" class="fa fa-spinner fa-pulse fa-fw fa-3x"></i></div>`
 })
 
 export class SpinnerComponent {
 
-@Input() visible = true;
+	@Input() visible = true;
 
 }
