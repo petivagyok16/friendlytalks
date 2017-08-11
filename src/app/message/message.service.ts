429ea20840255913bf341b813077f00aeb440c01
peter palocz <peter.palocz91@gmail.com>
2017-08-11 19:28:30 +0200

    refactor(messageService): refactor to forEach

--- src/app/message/message.service.ts
+++ src/app/message/message.service.ts
@@ -24,16 +24,15 @@ export class MessageService {
 	getMessages(skipper) {
 		return this.networkService.get(`message/${skipper}`)
 			.map((response: Response) => {
-				const DATA = response.json().obj;
-				console.log(DATA);
-				const objs: any[] = [];
+				const messagesObj = response.json().obj;
+				const messages: any[] = [];
 
-				for (let i = 0; i < DATA.length; i++) {
-					const message = new Message(DATA[i].content, DATA[i].created_at, DATA[i].user.username, DATA[i].meta,
-						DATA[i]._id, DATA[i].user._id, DATA[i].user.pictureUrl);
-					objs.push(message);
-				}
-				return objs;
+				messagesObj.forEach(message => {
+					const messageObject = new Message(message.content, message.created_at, message.user.username, message.meta,
+						message._id, message.user._id, message.user.pictureUrl);
+					messages.push(messageObject);
+				}) 
+				return messages;
 			})
 			.catch((error: Response) => Observable.throw(error.json()));
 	}
@@ -48,7 +47,6 @@ export class MessageService {
 
 	editMessage(message: Message) {
 		const BODY = JSON.stringify(message);
-		const HEADERS = new Headers({ 'Content-Type': 'application/json' });
 
 		return this.networkService.patch(`message/${message.messageId}`, BODY)
 			.map((response: Response) => response.json())
@@ -56,9 +54,7 @@ export class MessageService {
 	}
 
 	rateMessage(messageId, raterUserId, rating, prevRating) {
-		// console.log('messageId: ' + messageId, 'userId: ' + raterUserId, 'rating: ' + rating);
 		const RATINGOBJECT = JSON.stringify({ raterUserId: raterUserId, rating: rating, prevRating: prevRating });
-		const HEADERS = new Headers({ 'Content-Type': 'application/json' });
 
 		return this.networkService.patch(`message/rate/${messageId}`, RATINGOBJECT)
 			.map((response: Response) => console.log(response.json()))
